22ade240da8f34dc413773220ce8a3008e8cea75
Anthony Robustelli <robo28734@gmail.com>
2023-02-24 15:36:11 -0500

    Update App.js
    Test2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Will update soon</p>
+        <p>Here is the update:</p>
         <p>Poop</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
       </header>
